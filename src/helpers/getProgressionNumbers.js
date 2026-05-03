cd18361d7d4d91119fd8a04c6a87429d4c0a5e50
Эдуард <edmart666@gmail.com>
2026-05-03 20:11:21 +0300

    what?

--- src/helpers/getProgressionNumbers.js
+++ src/helpers/getProgressionNumbers.js
@@ -1,6 +1,7 @@
 const getProgressionNumbers = (start, end, step) => {
 
     let result = new Array(end);
+    
     for (let i = 0; i < end; i++) {
         result[i] = start * step;
         start += 1;
